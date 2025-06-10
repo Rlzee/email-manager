f1618fb09c7b5360471962e0f17c9e1a0e6938a6
Rlzee <romainprovencel@icloud.com>
2025-06-10 18:08:23 +0200

    feat: add AnimatedGroup component for enhanced animations
    - Implemented AnimatedGroup component to provide various animation presets (fade, slide, scale, etc.) for child elements using framer-motion.
    - Created button component with customizable variants and sizes using class-variance-authority.
    - Developed HeroSection component with animated elements and background images for the landing page.
    - Added utility function `cn` for merging class names with Tailwind CSS.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Exo_2, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Exo_2({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
