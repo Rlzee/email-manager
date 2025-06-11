1a278f4918984ae4b8af8654459873f886ab997b
Rlzee <romainprovencel@icloud.com>
2025-06-11 16:49:43 +0200

    feat: add Pricing component to enhance subscription options on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import { HeroSection } from "@/features/landing/hero";
 import { Header } from "@/features/landing/header";
 import { Footer } from "@/features/landing/footer";
 import { Faq } from "@/features/landing/faq";
+import { Pricing } from "@/features/landing/pricing";
 
 export default function Home() {
   return (
     <>
       <Header />
       <HeroSection />
+      <Pricing />
       <Faq />
       <Footer />
     </>
